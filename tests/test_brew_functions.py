e9c25e537a55ed713d54b19e422af9af1d178c3d
Douglas Pizac <douglas.pizac@gmail.com>
2024-05-16 13:16:32 -0400

    Updated test function to handle multiple cases
    Used pytest.mark.parametrize decorator to test multiple cases in a single test function to adhere to single responsibility.

--- tests/test_brew_functions.py
+++ tests/test_brew_functions.py
@@ -1,8 +1,9 @@
 """pytest will execute all tests in all files 
 whose names follow the form:
-test_*.py or \*_test.py
+test_*.py or *_test.py
 in the current directory and its subdirectories"""
 import re
+import pytest
 def clean_abv(raw_abv: str)-> str:
     abv_final = re.match(pattern = "[0-9]+[.]*[0-9]*",
                                string = raw_abv).group(0)
@@ -10,7 +11,10 @@ def clean_abv(raw_abv: str)-> str:
 
 # Run the following within the command line
 # pytest test_brew_functions.py::test_clean_abv
-def test_clean_abv():
-    assert clean_abv('9.5% ABV') == '9.5'
-    assert clean_abv('4.7 %') == '4.7'
-    assert clean_abv('5.2 % abv') == '5.2'
+@pytest.mark.parametrize("raw_abv, expected_abv",  
+  [("9.5% ABV", "9.5"),  
+   ("4.7 %", "4.7"),
+   ("5.2 % abv","5.2")])
+def test_clean_abv(raw_abv, expected_abv):
+    cleaned_abv = clean_abv(raw_abv)
+    assert cleaned_abv == expected_abv
